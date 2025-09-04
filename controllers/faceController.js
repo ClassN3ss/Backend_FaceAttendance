88fc622fe611e5d94862a6f3a4288846b4d930dc
PathaninThima <nattawut.thima@gmail.com>
2025-09-04 15:58:00 +0700

    edit path

--- controllers/faceController.js
+++ controllers/faceController.js
@@ -127,7 +127,7 @@ exports.verifyTeacherFace = async (req, res) => {
     }
 
     // 2) ส่งรูปไปหา Model เพื่อ encode
-    const MODEL_BASE_URL = process.env.MODEL_BASE_URL || "https://face-api-md-97765e8728dc.herokuapp.com/";
+    const MODEL_BASE_URL = process.env.MODEL_BASE_URL || "https://face-api-md-3cf4e65c1187.herokuapp.com/";
     const form = new (require("form-data"))();
     form.append("image", file.buffer, { filename: file.originalname, contentType: file.mimetype });
     form.append("fullname", fullname.trim());
