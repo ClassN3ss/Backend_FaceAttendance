eb1e91b7656be2a6c183a8509954ed06cfd05260
PathaninThima <nattawut.thima@gmail.com>
2025-09-05 08:41:11 +0700

    change path

--- controllers/authController.js
+++ controllers/authController.js
@@ -196,7 +196,7 @@ exports.saveTeacherFace = async (req, res) => {
     }
 
     // ✅ ส่งรูป + fullname ไปหา Model (/api/teacher-saveface)
-    const MODEL_BASE_URL = process.env.MODEL_BASE_URL || 'https://face-api-md-428d27208c88.herokuapp.com/';
+    const MODEL_BASE_URL = process.env.MODEL_BASE_URL || 'https://face-api-md-428d27208c88.herokuapp.com';
     const form = new FormData();
     form.append('image', file.buffer, { filename: file.originalname, contentType: file.mimetype });
     form.append('fullname', fullname.trim());
